a21a0bbd5367757b72c10181d9f004110b070cee
Sirius <83930223@qq.com>
2020-12-20 17:29:16 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 yarn build &&
 git checkout gh-pages &&
-rm -rf src *.html *.css *.png *.js &&
+rm -rf *.html *.css *.png *.js &&
 mv dist/* ./ &&
 rm -rf dist;
 git add . && 
 git commit -m 'update' &&
 git push 
+git checkout -
